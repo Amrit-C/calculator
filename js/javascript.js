a2285946b2d82c9475d1c67261cd4c23544fb2a8
Amrit Clair <amrit_clair@hotmail.com>
2022-04-30 18:51:15 -0400

    Basic calculator function and display completed.

--- js/javascript.js
+++ js/javascript.js
@@ -31,3 +31,64 @@ function operate(operator, a, b){
         return divide(a,b); 
     }
 }
+
+//Object Constructor for display value. 
+function Output (num1, num2, operator){
+    this.num1 = num1; 
+    this.num2 = num2; 
+    this.operator = operator; 
+}
+
+const display = new Output(1,2,3);
+
+//Add listener to digit buttons
+const digitClicked = document.querySelectorAll('.digits');
+const outputDisplay = document.querySelector('.output-display');
+const operatorClicked = document.querySelectorAll('.operators');
+const equalSign = document.getElementById('equals');
+
+digitClicked.forEach(addingEventListenerToDigits);
+operatorClicked.forEach(addingEventListenerToOperators);
+
+//Variables for first digit, second digit, operator.
+let firstDigit; 
+let secondDigit; 
+let operator;
+
+//Display digits on the output and store values.
+function addingEventListenerToDigits(digit){
+    digit.addEventListener('click', function (e){
+        outputDisplay.innerHTML = digit.textContent;
+        if (!firstDigit){
+            firstDigit = Number(digit.textContent);
+        }else{
+            secondDigit = Number(digit.textContent);
+        }
+    })
+}
+//Clear button restores nothing
+const clearBtn = document.getElementById('clear');
+clearBtn.addEventListener('click', e => {
+    outputDisplay.textContent = ''; 
+    firstDigit = 0; 
+    secondDigit = 0;
+})
+
+//Operator Selection
+function addingEventListenerToOperators(operators){
+    operators.addEventListener('click', function (e){
+        switch(operators.id){
+            case 'subtract': operator ='subtract'; break;
+            case 'add': operator = 'add'; break;
+            case 'multiply': operator = 'multiply'; break;
+            case 'divide': operator = 'divide'; break;
+        }
+    })
+}
+
+//Equal sign and display answer
+equalSign.addEventListener('click', (e) => {
+    firstDigit = Number(operate(operator, firstDigit, secondDigit));
+    outputDisplay.textContent = firstDigit;
+})
+
